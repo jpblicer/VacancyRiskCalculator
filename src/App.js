2577316233bdb2301c162d38608305fd44add2ee
jpblicer <jpblicer@gmail.com>
2023-09-09 14:56:30 +0900

    Creat CalculateRisk component and adjust CSS. Wrap elements in form tag for app

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import CalculateFee from './components/ExpectedFee'
 import Headcount from './components/Headcount'
 import ThreeCriteria from './components/threeCriteria/ThreeCriteria'
 import AdditionalCriteria from './components/AdditionalCriteria';
+import CalculateRisk from './components/CalculateRisk';
 
 
 function App() {
   return (
     <div className="App">
       <h1>Contingency Vacancy Risk Calculator</h1>
-      <Title />
-      <CalculateFee />
-      <Headcount />
-      <ThreeCriteria />
-      <AdditionalCriteria />
+      <form>
+        <Title />
+        <CalculateFee />
+        <Headcount />
+        <ThreeCriteria />
+        <AdditionalCriteria />
+      </form>
+      <CalculateRisk />
     </div>
   );
 }
---
+++ src/components/CalculateRisk.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function CalculateRisk(){
+    return(
+        <>
+            <h2>This Vacancy has a XX% chance to be filled</h2>
+        </>
+    );
+}
+
+
+export default CalculateRisk;
