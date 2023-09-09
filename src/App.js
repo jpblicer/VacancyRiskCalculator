39d3a4a4d931ad6cfa2abaaf85e31912cf02356d
jpblicer <jpblicer@gmail.com>
2023-09-09 13:16:27 +0900

    Created Component VacancyTitle

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Title from './components/VacacancyTitle'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Contingency Vacancy Risk Calculator</h1>
+      <Title />
     </div>
   );
 }
---
+++ src/components/VacacancyTitle.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Title(){
+    return(
+        <>
+            <p>Test</p>
+        </>
+    );
+}
+
+
+export default Title;
