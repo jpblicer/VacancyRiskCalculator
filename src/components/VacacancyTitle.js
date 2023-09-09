b38ca02b2f3d01e2195769f5639e294e86b79662
jpblicer <jpblicer@gmail.com>
2023-09-09 13:46:00 +0900

    create VacancyTitle input with basic css

--- src/components/VacacancyTitle.js
+++ src/components/VacacancyTitle.js
@@ -3,7 +3,14 @@ import React from "react";
 function Title(){
     return(
         <>
-            <p>Test</p>
+            <div class="vacancyTitle">
+                <label for="clientName">Client : </label>
+                <input name="clientName" type="text"></input>
+            </div>
+            <div class="vacancyTitle">
+                <label for="vacancyName">Vacancy : </label>
+                <input name="vacancyName" type="text"></input>
+            </div>
         </>
     );
 }
